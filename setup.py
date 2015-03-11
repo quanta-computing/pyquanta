b22bede705261ed3ba24b2294fc48e3048dcd3c2
Matthieu Rosinski <korrigan@slash16.org>
2015-03-11 15:14:41 +0100

    updated version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def requirements():
 
 
 setup(name='pyquanta',
-      version='0.1.1',
+      version='0.2.1',
       description='Pyquanta is a python module to interact with Quanta API',
       long_description=readme(),
       license='MIT',
